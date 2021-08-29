1fee44ffe8fec6a4d0dc7363566683be0c76e3fc
Evan Kerik <ekerik220@gmail.com>
2021-08-29 22:55:26 +0900

    Fix SEO issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "styles/globalStyles";
 import { ThemeProvider } from "styles/theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>ToA Handbook</title>
+        <meta
+          name="description"
+          content="Tales of Aselia guides and mechanics information."
+        />
+      </Head>
       <ThemeProvider />
       <GlobalStyles />
       <Component {...pageProps} />
